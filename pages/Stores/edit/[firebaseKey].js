89a2dd39e147f7ad1effaec4140aa138c9e8334d
drumstrum88 <rev.jsanderson@gmail.com>
2023-09-04 12:05:43 -0500

    updating styles

--- pages/Stores/edit/[firebaseKey].js
+++ pages/Stores/edit/[firebaseKey].js
@@ -2,13 +2,10 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { getSingleStore } from '../../../components/API/storeData';
 import StoreForm from '../../../components/storeForm';
-import { useAuth } from '../../../utils/context/authContext';
 
 export default function EditStore() {
   const [editStore, setEditStore] = useState({});
   const router = useRouter();
-  const [content, setContent] = useState(null);
-  const { user } = useAuth();
 
   const { firebaseKey } = router.query;
 
@@ -16,17 +13,9 @@ export default function EditStore() {
     getSingleStore(firebaseKey).then(setEditStore);
   }, [firebaseKey]);
 
-  useEffect(() => {
-    if (editStore) {
-      if (user && user.uid === editStore.uid) {
-        setContent(<StoreForm obj={editStore} />);
-      } else {
-        alert('You do not have permission to edit this store');
-      }
-    } else {
-      setContent(<p>Loading...</p>);
-    }
-  }, [editStore, user]);
-
-  return <div>{content}</div>;
+  return (
+    <div>
+      <StoreForm obj={editStore} />
+    </div>
+  );
 }
